81ed29faf6ce61ede13293f88cc73460847a740d
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2023-04-29 15:12:35 -0500

    useState

--- ecommerce/src/components/Nav.jsx
+++ ecommerce/src/components/Nav.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react"
 import { NavLink } from "react-router-dom"
 
 export default function Nav() {
+    const [mode, setMode] = useState("light")
+
+    function modeSwap() {
+        if (mode === "light") {
+            setMode("dark")
+        } else {
+            setMode("light")
+        }
+    }
+    
     return (
         <>
             <ul className="main-nav">
@@ -8,6 +19,7 @@ export default function Nav() {
                 <NavLink to="/about"><li className="main-nav-item">About</li></NavLink>
                 <NavLink to="/shop"><li className="main-nav-item">Shop</li></NavLink>
                 <NavLink to="/contact"><li className="main-nav-item">Contact</li></NavLink>
+                <li><button onClick={modeSwap}>Switch Mode: {mode}</button></li>
             </ul>
         </>
     )
